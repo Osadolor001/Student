82828caf53d8ebc92487cad512a53f84752e7c42
osadolor ebhuoma <ebhuoma@sheridancollege.ca>
2021-10-06 09:53:29 -0400

    First Commit

---
+++ src/student_list/Student.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student_list;
+
+/**
+ *
+ * @author Osadolor Ebhuoma
+ */
+public class Student {
+    
+    private String studentID;
+    private String studentName;
+
+    public Student(String studentID, String studentName) {
+        this.studentID = studentID;
+        this.studentName = studentName;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+    
+    
+    
+}

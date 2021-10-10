9d28bfb8b8da7ec07396f9608ce3f45f0cb0e495
osadolor ebhuoma <ebhuoma@sheridancollege.ca>
2021-10-09 21:20:42 -0400

    1st Commit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -3,7 +3,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author Osadolor Ebhuoma
  */
 public class CardTrick {
     public static void main(String[] args)
